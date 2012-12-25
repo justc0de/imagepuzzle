ab44e7a093d5c277ca0b5602483854f4283ca9ba
Dylan Byrne <dbyrne@hush.com>
2012-12-25 18:27:45 +0000

    User Interface - Change grid to a single standard layout (1/2 screen dimensions) . Added a warning message to inform users that images smaller than 1/4 of the screen dimesnions may loose quality when scaled

--- js/ResizeImage.js
+++ js/ResizeImage.js
@@ -18,22 +18,27 @@ var ResizeImage = {
 	
 		img.src	 = imgsrc;
 
-		//minimum image size
+		//show message to the user if the image is a small size
 		if(img.width < screen.width/4 || img.height < screen.height/4){
+			PicPuzzle_Utils.updateText('message','As the submitted image is small it may lose quality when scaled up');
 			
-			canvas.width = screen.width/4;
-			canvas.height = screen.height/4;
-			ctx.drawImage(img,0,0,screen.width/4,screen.height/4);
-		
+			//set message to show in case it has previously faded out
+		    $('#message').fadeTo('fast', 1)
+			
+			
+			//set message to fade out
+			setTimeout(function() {
+			    //$('#message').fadeOut('fast');
+				 $('#message').fadeTo('fast', 0)
+				}, 3000); // <-- time in milliseconds
 		}
-		else{
 
-			//maximum imaage size
-			canvas.width = screen.width/2;
-			canvas.height = screen.height/2;
-			ctx.drawImage(img,0,0,screen.width/2,screen.height/2); 
+		//maximum imaage size
+		canvas.width = screen.width/2;
+		canvas.height = screen.height/2;
+		ctx.drawImage(img,0,0,screen.width/2,screen.height/2); 
 		
-		}
+		
 	
 		return canvas;
 	}
